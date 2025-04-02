c68e81855a0051562b51398ee293e021994dd717
arpan-katiyar <arpankatiyar2002@gmail.com>
2025-04-02 22:07:01 +0530

    Updated footer with Link tags and Google Docs links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
   return (
@@ -14,10 +16,12 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>COMPANY</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
+          <li><Link to="/about" className="hover:underline">About us</Link></li>
+          <li><Link to="/contact" className="hover:underline">Contact us</Link></li>
+          <li><a href="https://docs.google.com/document/d/1_4Zoa4hLNQxxSlseDDluPf1-yGVtfGAJ8jkSK_LE1Wc/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:underline">Terms & Conditions</a></li>
+          <li><a href="https://docs.google.com/document/d/1pmcd_EurkCO8BhGCKxSkZRJHUpUGowSF6cnhiNpN_u8/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:underline">Refund Policy</a></li>
+          <li><a href="https://docs.google.com/document/d/1og8GvG3Yv9FzzJ2mo65dBsw2EUpUw2NQ1Z87sME_VSs/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:underline">Privacy Policy</a></li>
+          <li><a href="https://docs.google.com/document/d/1wiBWDUUV5Js3auIlUD5H84_T6TDeLwSJYTTyntq-KAU/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:underline">Pricing</a></li>
           </ul>
         </div>
 
